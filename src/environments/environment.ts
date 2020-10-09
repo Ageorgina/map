1e900e23a74f1280ed8eb68804b0f2e7056f0bf9
ageorgina <aortega@asgbpm.com>
2020-10-08 21:29:13 -0500

    archivos secciones .json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  // cartografiaUrl: 'https://s3.amazonaws.com/nullpointerexception.com',
+  //cartografiaUrl: 'https://s3.amazonaws.com/nullpointerexception.com',
   cartografiaUrl: 'https://www.entropia-ecosystem.com'
 };
 
