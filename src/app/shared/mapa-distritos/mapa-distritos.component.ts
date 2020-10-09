05dfd58cf6872df35ad960c78cbe185be08833a9
isidorotrevino <isidoro.trevino@gmail.com>
2020-10-09 18:00:05 -0500

    Corrección de bug para android

--- src/app/shared/mapa-distritos/mapa-distritos.component.ts
+++ src/app/shared/mapa-distritos/mapa-distritos.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnDestroy, OnInit} from '@angular/core';
 import * as Highcharts from 'highcharts/highmaps';
-import { EstadosService } from '../../general/services/estados.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { MenuService } from '../../general/services/menu.service';
+import {EstadosService} from '../../general/services/estados.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {MenuService} from '../../general/services/menu.service';
 //const datas = [['16', 16],['8', 8],['9',9],['10',10],['15',15]];
 declare var require: any;
 const Boost = require('highcharts/modules/boost');
 const noData = require('highcharts/modules/no-data-to-display');
 const More = require('highcharts/highcharts-more');
-
+const highchartsCustomEvents = require('highcharts-custom-events');
 Boost(Highcharts);
 noData(Highcharts);
 More(Highcharts);
 noData(Highcharts);
+highchartsCustomEvents(Highcharts);
 let $;
 var regex = /(\d+)/g;
 
@@ -21,7 +22,7 @@ var regex = /(\d+)/g;
   templateUrl: './mapa-distritos.component.html',
   styleUrls: ['./mapa-distritos.component.scss']
 })
-export class MapaDistritosComponent implements OnInit {
+export class MapaDistritosComponent implements OnInit, OnDestroy {
   distritoID: any;
   ejemplo: any;
   distritos: any;
@@ -31,85 +32,120 @@ export class MapaDistritosComponent implements OnInit {
   distValue: string;
   partidoValue: string;
   loading = true;
+  mapa: any = {
+    chart: {
+      backgroundColor: '#3F3F3F'
+    },
+    title: {text: ''},
+    mapNavigation: {
+      enabled: true,
+      buttonOptions: {
+        verticalAlign: 'bottom'
+      }
+    },
+    colorAxis: {
+      tickPixelInterval: 300,
+      min: 0,
+      minColor: '#E6E7E8',
+      maxColor: '#db6904'
+    },
+    plotOptions: {
+      map: {
+        point: {
+          events: {
+            click: (e) => {
+              console.log('click ', e, this);
+              /* tslint:disable:no-string-literal */
+              window['angularComponentRef'].zone.run(() => {
+                if (e.point && e.point.DISTRITO_L) {
+                  window['angularComponentRef'].component.selected(e.point.DISTRITO_L);
+                }
+              });
+              /* tslint:enable:no-string-literal */
+            }
+          }
+        }
+      }
+    },
+    tooltip: {
+      headerFormat: '<br><b>Saltillo D XVI</b><br>',
+      pointFormat: '<br>' +
+        '<b><b><br>' +
+        '<b>Preocupaciones:<b><br>' +
+        'Recuperación Económica<br>' +
+        'Corrupción<br>' +
+        'Brutalidad Policiaca<br>' +
+        'Feminicidios<br>' +
+        'Salud<br>'
+    },
+    series: [{
 
-  constructor( private estados: EstadosService, private menuSrv: MenuService, private router: Router) {
-    this.distValue = this.estados.getCOOKIE().match(regex).toString();
-    this.menuSrv.getInfoDistritos(this.distValue).subscribe( info => this.ejemplo = info[0]);
-    this.distValue = this.distValue.replace(/\b0+/g, "");
-
-                this.menuSrv.getDistritos().subscribe( distritos => {
-                  this.distritos = distritos;
-                });
-                this.estados.getMapaDistritos(this.estado).subscribe(data => {
-                  data.filter( x => {
-                    if (x[0] === this.distValue) {
-                      data = [x];
-                      this.distritosMapas = data;
-                    }
-                  });
-                });
-   }
-
-    mapa: any = {
-      chart: {
-        backgroundColor: '#3F3F3F'
-      },
-      title: { text: '' },
-      mapNavigation: {
+      keys: ['DISTRITO_L', 'value'],
+      joinBy: ['DISTRITO_L'],
+      dataLabels: {
         enabled: true,
-        buttonOptions: {
-          verticalAlign: 'bottom'
-               }
-           },
-          colorAxis: {
-            tickPixelInterval: 300,
-            min: 0,
-            minColor: '#E6E7E8',
-          maxColor: '#db6904'
-     },
-     tooltip: {
-         headerFormat: '<br><b>Saltillo D XVI</b><br>',
-         pointFormat: '<br>' +
-             '<b><b><br>' +
-             '<b>Preocupaciones:<b><br>' +
-             'Recuperación Económica<br>' +
-             'Corrupción<br>' +
-             'Brutalidad Policiaca<br>' +
-             'Feminicidios<br>' +
-             'Salud<br>'
-     },
-       series: [{
+        color: '#FFFFFF',
+        fill: '#f1f7ff',
+        format: '{point.properties.DISTRITO_L}',
+        formatter() {
+          if (this.point.value) {
+            return this.point.name;
+          }
+        }
+      },
+    }]
+  };
 
-            keys: ['DISTRITO_L', 'value'],
-            joinBy: ['DISTRITO_L'],
-           dataLabels: {
-                 enabled: true,
-                color: '#FFFFFF',
-               fill: '#f1f7ff',
-               format: '{point.properties.DISTRITO_L}',
-               formatter() {
-                     if (this.point.value) {
-                         return this.point.name;
-                  }
-              }
-          },
-      }]
-   };
+  constructor(private estados: EstadosService, private menuSrv: MenuService, private router: Router, private ngZone: NgZone) {
+    this.distValue = this.estados.getCOOKIE().match(regex).toString();
+    this.menuSrv.getInfoDistritos(this.distValue).subscribe(info => this.ejemplo = info[0]);
+    this.distValue = this.distValue.replace(/\b0+/g, '');
 
-  ngOnInit() {
-      this.estados.getDistritos().subscribe( entidades => {
-        this.construirMapa(entidades).finally(() => {this.loading = false; } );
-      } );
-    }
-
-    async construirMapa(entidadesJSON) {
-      this.mapa.chart.map = entidadesJSON;
-      this.mapa.series[0].data = this.distritosMapas;
-      Highcharts.mapChart('estado', this.mapa);
-    }
-    selected(id) {
-      if (id === null) { return ; }
-      this.router.navigate(['secciones', id]);
+    this.menuSrv.getDistritos().subscribe(distritos => {
+      this.distritos = distritos;
+    });
+    this.estados.getMapaDistritos(this.estado).subscribe(data => {
+      data.filter(x => {
+        if (x[0] === this.distValue) {
+          data = [x];
+          this.distritosMapas = data;
+        }
+      });
+    });
+    /* tslint:disable:no-string-literal */
+    window['angularComponentRef'] = {component: this, zone: ngZone};
+    /* tslint:enable:no-string-literal */
   }
 
+  ngOnInit() {
+    this.estados.getDistritos().subscribe(entidades => {
+      this.construirMapa(entidades).finally(() => {
+        this.loading = false;
+      });
+    });
+  }
+
+  async construirMapa(entidadesJSON) {
+    this.mapa.chart.map = entidadesJSON;
+    this.mapa.series[0].data = this.distritosMapas;
+    Highcharts.mapChart('estado', this.mapa);
+  }
+
+  selected(id, $event) {
+    console.log('id ', id, this.distValue == id, this.distValue);
+    const cook = this.estados.getCOOKIE();
+    if (id === null) {
+      return;
+    }
+    if (this.distValue == id) {
+      this.router.navigate(['secciones', id]);
+    }
+
+  }
+
+  ngOnDestroy() {
+    /* tslint:disable:no-string-literal */
+    window['angularComponentRef'] = null;
+    /* tslint:enable:no-string-literal */
+  }
 }
