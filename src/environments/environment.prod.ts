470ae4c3f890d7f855f4f5870b02625bea7c89a7
isidorotrevino <isidoro.trevino@gmail.com>
2020-10-24 09:14:07 -0500

    Ajuste a config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   //cartografiaUrl: 'https://s3.amazonaws.com/nullpointerexception.com',
-  // cartografiaUrl: 'https://www.entropia-ecosystem.com',
+  cartografiaUrl: 'https://www.entropia-ecosystem.com',
   cartografiaBack: 'https://auth.entropia-ecosystem.com'
 };
